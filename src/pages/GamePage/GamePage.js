b0ddea3044c965ae0972c2eefbabd9b24cd8405e
Kod-dasm <54396012+Kod-dasm@users.noreply.github.com>
2021-11-09 22:24:04 +0500

    updated design, created button update field, button new game, panel score and timer , added win lost

--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -11,7 +11,16 @@ export default {
             sizePolyX: 10,
             sizePolyY: 10,
             minStar: 2,
-            color: ['blue', 'green', 'purple', 'red', 'yellow'],
+            score: 0,
+            needScore: 1000,
+            time: 100,
+            currentTimer: 0,
+            timer: null,
+            gameStart: false,
+            win: false,
+            lost: false,
+            countUpdateField: 3,
+            pause: false,
         }
     },
     methods: {
@@ -23,12 +32,12 @@ export default {
         addCell() {
             let cells = []
             for (let i = 0; i < this.sizePolyY; i++) {
-                cells.push({ color: this.color[this.addRandomColor()], active: false, mistake: false })
+                cells.push({ color: this.addRandomColor(), active: false, mistake: false })
             }
             return cells
         },
         addRandomColor() {
-            return Math.floor(Math.random()*5)
+            return ['blue', 'green', 'purple', 'red', 'yellow'][Math.floor(Math.random()*5)]
         },
         recoverPoly() {
             for (let x = 0; x < this.sizePolyX; x++) {
@@ -38,21 +47,23 @@ export default {
             }
         },
         addStar(index) {
-            this.poly[index].push({ color: this.color[this.addRandomColor()], active: false, mistake: false })
+            this.poly[index].push({ color: this.addRandomColor(), active: false, mistake: false })
         },
-        burnStar(coordX, coordY) {
-            this.poly[coordX].splice(coordY, 1)
+        burnStars() {
+            setTimeout(()=>{
+                this.poly = this.poly.map(itemX => itemX.filter(itemY => !itemY.active ? true : false))
+                this.recoverPoly()
+            }, 200)
         },
-        removeStar(item, coordX, coordY) {
-            // console.log(item.color, coordX, coordY)
-            // console.log(this.poly[coordX][coordY].active)
+        removeStars(item, coordX, coordY) {
             this.searchInWidth(item.color, coordX, coordY)
-            console.log('PolyDel fin = ',this.polyDel)
-            if (this.polyDel.length > this.minStar) {
-                setTimeout(()=>{
-                    this.poly = this.poly.map(itemX => itemX.filter(itemY => !itemY.active ? true : false))
-                    this.recoverPoly()
-                }, 200)
+            if (this.polyDel.length >= this.minStar) {
+                this.burnStars()
+                this.score += this.countingScore(this.polyDel.length)
+                if (this.score >= this.needScore) {
+                    this.win = true
+                    this.stopTimer()
+                }
             }
             else {
                 this.poly[coordX][coordY].mistake = true
@@ -86,14 +97,80 @@ export default {
                 this.searchInWidth(color, coordX + 1, coordY)
             }
         },
+        countingScore(count) {
+            if (count >= this.minStar) {
+                return count + this.countingScore(count - 1)
+            }
+            return 0
+        },
+        startGame() {
+            if (!this.gameStart) {
+                this.gameStart = true
+                this.currentTimer = this.time
+                this.startTimer()
+            }
+        },
+        startTimer(){
+            this.timer = setInterval(() => {
+                this.currentTimer > 0 ? this.currentTimer-- : this.lost = true
+            }, 1000)
+        },
+        stopTimer(){
+            clearTimeout(this.timer)
+        },
+        updateField() {
+            if (this.countUpdateField > 0 && this.gameStart && !this.pause && !this.win && !this.lost) {
+                this.poly = []
+                this.createdPoly()
+                this.countUpdateField--
+            } 
+            // else {
+
+            // }
+        },
+        startNewGame() {
+            this.stopTimer()
+            this.currentTimer = this.time
+            this.poly = []
+            this.score = 0
+            this.countUpdateField = 3
+            this.gameStart = false
+            this.win = false
+            this.lost = false
+            this.createdPoly()
+        },
+        putPause() {
+            if (this.gameStart && !this.win && !this.lost) {
+                this.pause = !this.pause
+                this.pause ? this.stopTimer() : this.startTimer()
+            }
+        },
     },
     computed: {
-        watchPoly() {
+        showPoly() {
             console.log(this.poly)
             return this.poly
-        }
+        },
+        showScore() {
+            return this.score
+        },
+        showWin() {
+            return this.win
+        },
+        showLost() {
+            return this.lost
+        },
+        showCurrentTimer() {
+            return this.currentTimer
+        },
+        showCountUpdateField() {
+            return this.countUpdateField
+        },
     },
     created: function() {
         this.createdPoly()
-    }
+    },
+    destroyed: function() {
+        this.stopTimer()
+    },
 }
